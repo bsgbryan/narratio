f8aa64d371f3e598e44d6991dd551ebbb23ed4c5
Bryan Maynard <bsgbryan@gmail.com>
2012-05-08 21:47:08 -0500

    Adding some logic

--- web.js
+++ web.js
@@ -1,12 +1,45 @@
-var express = require('express');
+var express = require('express'),
+    redis   = require('redis-url').connect(process.env.REDISTOGO_URL),
+    app     = express.createServer(express.logger())
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.configure(function () {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static('public'));
+  
+  app.set('views', 'views');
+  app.set('view engine', 'jade');
 });
 
-var port = process.env.PORT || 3000;
+app.get('/', function(req, res) {
+  var last = redis.get('posts')
+
+  res.render('index')
+})
+
+app.get('/:id', function(req, res) {
+  var id   = req.params('id'),
+      post = redis.get('post-' + id)
+      
+  res.contentType('text/plain')
+  res.send(post)
+})
+
+app.get('new', function(req, res) {
+  res.render('create')
+})
+
+app.post('new', function(req, res) {
+  var index = redis.incr('posts')
+
+  redis.set('post-' + index, req.body.content)
+
+  res.send()
+})
+
+var port = process.env.PORT || 3000
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  console.log("Listening on " + port)
+})
