d73799eb9c54d2ad405a5344598015cfbb99820b
Bryan Maynard <bsgbryan@gmail.com>
2012-05-09 22:32:35 -0500

    Got redis working

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express'),
-    app     = express.createServer(express.logger())
+    app     = express.createServer(express.logger()),
+    sha1    = require('sha1')
 
 // Production
 if (process.env.REDISTOGO_URL) 
   var redis = require('redis-url').connect(process.env.REDISTOGO_URL)
 // Development
 else 
-  var redis = require('redis').createClient('localhost', 6379)
+  var redis = require('redis').createClient()
 
 app.configure(function () {
   app.use(express.methodOverride());
@@ -22,22 +23,36 @@ app.get('/', function(req, res) {
   res.render('index')
 })
 
-app.get('/post/:id', function(req, res) {
-  var id   = req.params('id'),
-      post = redis.get('post-' + id)
+app.get('/delete/:collection', function(req, res) {
+  redis.del(req.param('collection'), function(err, data) {
+    if (err)
+      res.status(500, err)
+    else 
+      res.status(200, 'deleted')
 
-  res.contentType('text/plain')
-  res.send(post)
+    res.end()
+  })
 })
 
-app.get('/new', function(req, res) {
+app.get('/blog.post/read/:index', function(req, res) {
+  var id = req.param('index') * -1 - 1
+
+  redis.lrange('blog.posts', id, id, function(err, data) {
+    var post = data[0]
+
+    res.write(post)
+    res.end()
+  })
+
+})
+
+app.get('/blog.post/write', function(req, res) {
   res.render('create')
 })
 
-app.post('/new', function(req, res) {
-  var index = redis.incr('posts')
-
-  redis.set('post-' + index, req.body.content)
+app.post('/blog.post/publish', function(req, res) {
+  redis.lpush('blog.posts',  req.body.content)
+  redis.lpush('blog.titles', req.body.title)
 
   res.send()
 })
