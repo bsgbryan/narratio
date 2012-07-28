45e4d1a5f24abd8876844a1c84247de6511278a8
Bryan Maynard <bsgbryan@gmail.com>
2012-07-28 17:52:47 -0500

    Updated styles. Added some support for supplemental content.

--- public/read.js
+++ public/read.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   $('#post').
     html(new Showdown.converter().makeHtml($('#content').text())).
-    prepend('<h1>' + $('#title').text() + '</h1>')
+    prepend('<h1>' + $('#title').text() + '</h1>').
+    prepend('<ul class="supplemental">' +
+      '<li>' +
+        // '<img src="http://timeentertainment.files.wordpress.com/2012/04/brave.jpg">' +
+      '</li>' +
+    '</ul>')
 })
--- public/write.js
+++ public/write.js
@@ -12,6 +12,11 @@ $(document).ready(function() {
     $('#preview').
       html(preview).
       prepend('<h1>' + title + '</h1>').
+      prepend('<ul class="supplemental">' +
+        '<li>' +
+          // '<img src="http://timeentertainment.files.wordpress.com/2012/04/brave.jpg">' +
+        '</li>' +
+      '</ul>')
       find('p > br').
       remove().
     end().
