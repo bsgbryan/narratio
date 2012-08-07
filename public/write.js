ac1c2e461a9a44a7c286d8f8a1ec81ba6ccb9ab1
Bryan Maynard <bsgbryan@gmail.com>
2012-08-07 01:06:34 -0500

    Rebuilt around a real time preview model

--- public/write.js
+++ public/write.js
@@ -1,39 +1,47 @@
-$(document).ready(function() {
-  $('#post').on('click', '.preview', function() {
-    var title   = $('.title').val(),
-        content = $('.content').val()
+(function ($) {
+  function resizeTextarea() {
+    var newContent = $('.new.content');
+    function resize () {
+      var rows = Math.ceil(newContent.val().length / 50)
+      var nls  = newContent.val().split("\n")
 
-    $('#post').addClass('hidden')
+      if (nls.length > 1 && (newContent.val()[0] === '-' || parseInt(newContent.val()[0]) !== NaN) && nls.length > rows)
+        rows = nls.length
 
-    var preview = new Showdown.
-      converter().
-      makeHtml(content)
+      newContent.css('height', (rows < 2 ? '1.3em' : 'auto'))
+      newContent.attr('rows', rows)
+    }
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    newContent.on('change',  resize);
+    newContent.on('cut',     delayedResize);
+    newContent.on('paste',   delayedResize);
+    newContent.on('drop',    delayedResize);
+    newContent.on('keydown', delayedResize);
 
-    $('#preview').
-      html(preview).
-      prepend('<h1>' + title + '</h1>').
-      prepend('<ul class="supplemental">' +
-        '<li>' +
-          // '<img src="http://timeentertainment.files.wordpress.com/2012/04/brave.jpg">' +
-        '</li>' +
-      '</ul>').
-      find('p > br').
-      remove()
-      
-    $('#preview').
-      removeClass('hidden')
+    newContent.focus();
+    newContent.select();
+    resize();
+  }
 
-    $('#back').removeClass('hidden')
+  $(document).ready(function() {
+    resizeTextarea()
 
-    return false
+    $('.new.content').on('keyup', function (event) {
+      console.log($(this).val().lastIndexOf("\n\n"))
+      console.log($(this).val().length - 2)
+      if (event.keyCode === 13 && $(this).val().lastIndexOf("\n\n") === $(this).val().length - 2) {
+        var content = new Showdown.
+          converter().
+          makeHtml($(this).val())
+
+        $(this).
+          before(content).
+          val('').
+          trigger('change')
+      }
+    })
   })
-
-  $('#back').on('click', function() {
-    $('#preview').addClass('hidden')
-    $('#back').addClass('hidden')
-    $('#post').removeClass('hidden')
-  })
-
-  $('.title').val($('#title').text())
-  $('.content').val($('#content').text())
-})
+})($)
