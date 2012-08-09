a73a47974e8c58d7c7a0dc4506c5227060a12c50
Bryan Maynard <bsgbryan@gmail.com>
2012-08-09 00:56:08 -0400

    Posts can now be edited inline. Content is created in chunks, and posts are live-previewed.

--- public/write.js
+++ public/write.js
@@ -1,47 +1,100 @@
 (function ($) {
-  function resizeTextarea() {
-    var newContent = $('.new.content');
+  var months   = [ 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec' ],
+      contents = { }
+  
+  function resizeTextarea(content) {
+    var newContent = $(content)
+    
     function resize () {
       var rows = Math.ceil(newContent.val().length / 50)
       var nls  = newContent.val().split("\n")
 
-      if (nls.length > 1 && (newContent.val()[0] === '-' || parseInt(newContent.val()[0]) !== NaN) && nls.length > rows)
+      if (nls.length > 1 && (newContent.val()[0] === '-' || !isNaN(newContent.val()[0])) && nls.length > rows)
         rows = nls.length
+      else if (newContent.val()[newContent.val().length - 1] === "\n")
+        ++rows
 
       newContent.css('height', (rows < 2 ? '1.3em' : 'auto'))
       newContent.attr('rows', rows)
     }
     /* 0-timeout to get the already changed text */
     function delayedResize () {
-        window.setTimeout(resize, 0);
+        window.setTimeout(resize, 0)
     }
-    newContent.on('change',  resize);
-    newContent.on('cut',     delayedResize);
-    newContent.on('paste',   delayedResize);
-    newContent.on('drop',    delayedResize);
-    newContent.on('keydown', delayedResize);
+    newContent.on('change',  resize)
+    newContent.on('cut',     delayedResize)
+    newContent.on('paste',   delayedResize)
+    newContent.on('drop',    delayedResize)
+    newContent.on('keydown', delayedResize)
 
-    newContent.focus();
-    newContent.select();
-    resize();
+    newContent.focus()
+    newContent.select()
+    resize()
   }
 
   $(document).ready(function() {
-    resizeTextarea()
+    resizeTextarea('.new.content')
+        
+    var date      = new Date(),
+        stamp     = date.getFullYear() + '-' + months[date.getMonth()] + '-' + date.getDate(),
+        host      = window.location.host.replace(/\W/g, '-'),
+        url       = 'https://gamma.firebase.com/bsgbryan/aioim/' + host + '/' + stamp, 
+        firebase  = new Firebase(url)
+
+    firebase.on('child_added', function (data) {
+      var content = new Showdown.
+        converter().
+        makeHtml(data.val().content)
+        
+      $('.new.content').
+        before(content).
+        prev().
+        append('<a href="' + url + '/' + data.name() + '" data-edit="' + data.name() + '" class="edit content">&</a>').
+        attr('id', data.name())
+      
+      contents[data.name()] = data.val()
+    })
 
     $('.new.content').on('keyup', function (event) {
-      console.log($(this).val().lastIndexOf("\n\n"))
-      console.log($(this).val().length - 2)
       if (event.keyCode === 13 && $(this).val().lastIndexOf("\n\n") === $(this).val().length - 2) {
-        var content = new Showdown.
-          converter().
-          makeHtml($(this).val())
+        firebase.push({ content: $(this).val() })
 
         $(this).
-          before(content).
           val('').
           trigger('change')
       }
+    })
+    
+    $('#post').on('click', '.edit.content', function (event) {
+      var e = $(event.currentTarget)
+      e.parent().replaceWith('<textarea id="' + e.data('edit') + '-editor" class="content editor">' + contents[e.data('edit')].content + '</textarea>')
+      resizeTextarea('#' + e.data('edit') + '-editor')
+      
+      $('#' + e.data('edit') + '-editor').on('keyup', function (event) {
+        if (event.keyCode === 13 && $(this).val().lastIndexOf("\n\n") === $(this).val().length - 2) {
+          var id      = e.data('edit'),
+              value   = $(this).val(),
+              prose   = new Showdown.
+                converter().
+                makeHtml(value)
+          
+          firebase.
+            child(id).
+            set({ 
+              content: value
+            })
+          
+          $(this).replaceWith($(prose).attr('id', id))
+          
+          $('#' + id).
+            append('<a href="' + url + '/' + id + '" data-edit="' + id + '" class="edit content">&</a>').
+            attr('id', id)
+          
+          contents[id] = { content: value }
+        }
+      })
+      
+      return false
     })
   })
 })($)
--- web.js
+++ web.js
@@ -1,15 +1,5 @@
 var express = require('express'),
-    app     = express.createServer(express.logger()),
-    sha1    = require('sha1')
-
-// Production
-if (process.env.REDISTOGO_URL) 
-  var redis = require('redis-url').connect(process.env.REDISTOGO_URL)
-// Development
-else 
-  var redis = require('redis').createClient()
-
-var gh = 'http://bsgbryan.github.com/blog/public/'
+    app     = express.createServer(express.logger())
 
 app.configure(function () {
   app.use(express.methodOverride());
@@ -21,56 +11,8 @@ app.configure(function () {
   app.set('view engine', 'jade');
 });
 
-app.get('/', function(req, res) {
-  redis.lrange('blog.posts', 0, 0, function(err, data) {
-    res.render('read', { action : 'read' , post: data })
-  })
-})
-
-app.get('/delete/:collection', function(req, res) {
-  redis.del(req.param('collection'), function(err, data) {
-    if (err)
-      res.status(500, err)
-    else 
-      res.status(200, 'deleted')
-
-    res.end()
-  })
-})
-
-app.get('/read/:post', function(req, res) {
-  var href = req.headers.host.indexOf('localhost') === 0 ? '/' : gh
-
-  redis.hgetall(':post:' + req.param('post'), function(err, data) {
-    res.render('read', { action: 'read', post: data, base: href })
-  })
-})
-
 app.get('/write', function(req, res) {
-  var href = req.headers.host.indexOf('localhost') === 0 ? '/' : gh
-
-  res.render('write', { action : 'write' , base : href })
-})
-
-app.get('/edit/:post', function(req, res) {
-  var href = req.headers.host.indexOf('localhost') === 0 ? '/' : gh
-
-  redis.hgetall(':post:' + req.param('post'), function(err, data) {
-    res.render('write', { action : 'write' , post : data , base :href })
-  })
-})
-
-app.post('/post/publish', function(req, res) {
-  var post = { 
-    id        : req.body.title.toLowerCase().replace(/\s/g, '-'),
-    title     : req.body.title, 
-    content   : req.body.content,
-    published : new Date()
-  }
-
-  var status = redis.hmset(':post:' + post.title.toLowerCase().replace(/\s/g, '-'), post)
-
-  res.send(status)
+  res.render('write', { action : 'write' })
 })
 
 var port = process.env.PORT || 3000
