7d3a30996acfcb66d5a92dc39da5f716c8059bc6
Bryan Maynard <bsgbryan@gmail.com>
2012-08-19 01:01:40 -0400

    Trying to get tests servers working together

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    app     = express.createServer(express.logger())
+    app     = express.createServer(express.logger()),
+    port    = process.env.PORT || 3000
 
 app.configure(function () {
   app.use(express.methodOverride());
@@ -12,10 +13,21 @@ app.configure(function () {
 });
 
 app.get('/', function(req, res) {
-  res.render('index', { layout: false })
+  var host   = '',
+      scribo = ''
+  
+  console.log('req', req)
+  
+  if (req.headers.host.indexOf('localhost') === 0) {
+    host   = 'localhost:' + port
+    scribo = 'scribo.jit.su'
+  } else {
+    host   = req.headers.host
+    scribo = 'project-livec9a3f7456521.rhcloud.com'
+  }
+    
+  res.render('index', { layout: false, host: host, scribo: scribo })
 })
-
-var port = process.env.PORT || 3000
 
 app.listen(port, function() {
   console.log("Listening on " + port)
